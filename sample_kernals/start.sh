4816c8faf71e7c73ec696fae3ef7dd967347f6ca
Alex Tong <alexanderytong@gmail.com>
2017-04-03 15:46:00 -0400

    added full_preprocess, which includes linear interpolation and thresholding

--- sample_kernals/start.sh
+++ sample_kernals/start.sh
@@ -1,1 +1,1 @@
-docker run -p 8888:8888 -v $PWD/../input:/tmp/input -v $PWD:/tmp/working -w=/tmp/working --rm -it kaggle/python 
+docker run -p 8889:8888 -v $PWD/../input:/tmp/input -v $PWD:/tmp/working -w=/tmp/working --rm -it kaggle/python 
