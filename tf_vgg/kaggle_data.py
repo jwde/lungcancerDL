8549acbe7041c8e048f319cfad5e9d60f6589a59
Jay <jaydestories@gmail.com>
2017-04-04 00:35:20 -0400

    hack to fix missing features tensors

--- tf_vgg/kaggle_data.py
+++ tf_vgg/kaggle_data.py
@@ -6,6 +6,7 @@ import numpy as np
 DATA_DIR = '/notebooks/sharedfolder/lungcancerdl/input/'
 lungs_dir = DATA_DIR + '3Darrays_visual/'
 labels_file = DATA_DIR + 'stage1_labels.csv'
+vgg_features_dir = DATA_DIR + 'vgg_training/'
 
 def get_labels_by_name():
     if not hasattr(get_labels_by_name, 'labels_by_name'):
@@ -13,7 +14,8 @@ def get_labels_by_name():
         with open(labels_file, 'rb') as csv_labels:
             labels_reader = csv.reader(csv_labels)
             for row in islice(labels_reader, 1, None):
-                get_labels_by_name.labels_by_name[row[0]] = int(row[1])
+                if not row[0] == '059d8c14b2256a2ba4e38ac511700203' and not row[0] == '66b7666912e1d469cd9817a1cade694c':
+                    get_labels_by_name.labels_by_name[row[0]] = int(row[1])
     return get_labels_by_name.labels_by_name
 
 def get_training_lungs():
@@ -43,3 +45,9 @@ def get_test_lungs():
             if not lung_id in get_labels_by_name():
                 lung_img = np.load(lungs_dir + f)
                 yield lung_img
+
+def get_training_vgg_features():
+    for lung_id in get_labels_by_name().keys():
+        f = lung_id + '.npy'
+        lung_features = np.load(vgg_features_dir + f)
+        yield lung_features, lung_id
